90c83fc25f470df01cda0a1b58493c63a360ab3a
jonbwalker <jonathan.b.walker0@gmail.com>
2020-08-29 13:07:28 -0700

    use out of the box cli coverage command and config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest',
-    collectCoverage: true,
-    collectCoverageFrom: [
-        'src/**/*.{js,vue}',
-        '!**/node_modules/**'
-    ],
     transformIgnorePatterns: ['/node_modules/'],
     roots: ["<rootDir>/src", "<rootDir>/tests"],
     testMatch: [
